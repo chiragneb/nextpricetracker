02af6e1ef04607761a2bcf9e5bc28e8134ddf458
neb03x <neb.3391@gmail.com>
2022-12-26 11:54:31 +0530

    fixing git issues

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,6 @@ export default function Home() {
         <title>nextpricetracker</title>
       </Head>
        <div className="mb-2 grid h-screen place-items-center">
-<<<<<<< HEAD
-           <h1 className="my-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
-                          Top 100 Cryptos! 💵 </h1>
-              <button className='mb-6 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' onClick={() => window.location.reload(false)}>Click to reload!</button>
-                 <CoinSearch />
-              <footer className="bg-gray-200 text-center lg:text-left my-8" >Built with 🖤 by 
-              <Link href={{ pathname: "https:chiragneb.vercel.app" }} target="_blank" className="font-bold underline mb-8"> Chirag Neb</Link></footer>
-=======
-
            <h1 className="my-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl dark:text-white">
                           Top 100 Cryptos! 💵 </h1>
               <div className='justify-center mb-4'>
@@ -31,7 +22,6 @@ export default function Home() {
               <footer className="text-center lg:text-left my-8" > 🏗️ by &nbsp;
               <Link href={{ pathname: "https:chiragneb.vercel.app" }} target="_blank" className="font-bold mb-8 underline">Chirag Neb</Link></footer>
               <div className='my-6 text-xs'>™️ 2023</div>
->>>>>>> darkmode
        </div>
     </>
   )
