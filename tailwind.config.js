71235948dbe49773161aa110e48e84c0fa62e727
neb03x <neb.3391@gmail.com>
2023-01-06 13:40:26 +0530

    some Minor changes - deleted Package-lock.json fixed tailwind config - added © on footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  purge: {
+  content: {
     enabled: true,
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
