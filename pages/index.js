f2b60f96c0067e375f43e6070b641cef71c2e5b5
neb03x <neb.3391@gmail.com>
2023-02-04 08:57:45 +0530

    Changed Heading of Site

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
         <title>nextpricetracker</title>
       </Head>
        <div className="mb-8 grid h-screen place-items-center">
-           <h1 className="mt-2 mb-6 text-4xl capitalize font-extrabold drop-shadow-2xl leading-none md:text-5xl lg:text-6xl dark:text-white">
-                          Top Cryptocurrency list </h1>
+           <h1 className="mt-2 mb-6 text-3xl capitalize font-extrabold drop-shadow-2xl leading-none md:text-5xl lg:text-6xl dark:text-white">
+                          CRYPTO PRICE TRACKER </h1>
               <div className='justify-center mb-4'>
               <button className='mr-2 bg-slate-500 text-white font-bold py-2 px-4 border-b-4 border-gray-700' onClick={() => window.location.reload(false)}> RELOAD </button>
               <button className='ml-2 bg-slate-500 font-bold py-2 px-4 border-b-4 border-gray-700' onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}> 🌓 </button>
